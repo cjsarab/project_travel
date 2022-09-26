11272034fe796bba96f947b0eb5ba624cd0c06fd
Cammy Smith <cjsarab1991@gmail.com>
2022-09-26 11:36:07 +0100

    Added html for cities

--- controllers/cities_controller.py
+++ controllers/cities_controller.py
@@ -1,6 +1,7 @@
 from http.client import USE_PROXY
 from flask import Flask, render_template, request, redirect, Blueprint
 from repositories import country_repository, city_repository
+from models.country import Country
 from models.city import City
 
 cities_blueprint = Blueprint("cities", __name__)
