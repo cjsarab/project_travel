a7afaa6a682e02950d6fb2336d8e4acb8bf8cc53
Cammy Smith <cjsarab1991@gmail.com>
2022-09-28 12:01:49 +0100

    Added tests

--- tests/sight_tests.py
+++ tests/sight_tests.py
@@ -3,6 +3,15 @@ import unittest
 from models.sight import Sight
 
 class TestSight(unittest.TestCase):
-
     def setUp(self):
-        pass
+        self.sight = Sight("Eiffel Tower", True, 1)
+
+    def test_sight_has_name(self):
+        self.assertEqual ("Eiffel Tower", self.sight.sight_name)
+
+    def test_sight_is_visited_true(self):
+        self.assertEqual (True, self.sight.is_visited)
+    
+    def test_sight_is_visited_false(self):
+        self.sight = Sight("Brandenburg Gate", False, 2)
+        self.assertEqual (False, self.sight.is_visited)
