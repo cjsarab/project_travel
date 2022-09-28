2a09b7231368ff8bbe8f54a27591e59011c6c1ab
Cammy Smith <cjsarab1991@gmail.com>
2022-09-28 10:46:22 +0100

    Changed facts to summary

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from controllers.sights_controller import sights_blueprint
 from controllers.cities_controller import cities_blueprint
 from controllers.countries_controller import countries_blueprint
-from controllers.facts_controller import facts_blueprint
+from controllers.summary_controller import summary_blueprint
 from controllers.quizzes_controller import quizzes_blueprint
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(sights_blueprint)
 app.register_blueprint(cities_blueprint)
 app.register_blueprint(countries_blueprint)
-app.register_blueprint(facts_blueprint)
+app.register_blueprint(summary_blueprint)
 app.register_blueprint(quizzes_blueprint)
 
 @app.route('/')
--- controllers/summary_controller.py
+++ controllers/summary_controller.py
@@ -1,11 +1,11 @@
 from http.client import USE_PROXY
-from flask import Flask, render_template, request, redirect, Blueprint
+from flask import Flask, render_template, Blueprint
 from repositories import country_repository, city_repository, sight_repository 
 
-facts_blueprint = Blueprint("facts", __name__)
+summary_blueprint = Blueprint("summary", __name__)
 
-@facts_blueprint.route("/facts")
-def facts():
+@summary_blueprint.route("/summary")
+def summary():
     total_countries = len(country_repository.select_all())
     total_cities = len(city_repository.select_all())
     total_sights = len(sight_repository.select_all())
@@ -17,5 +17,5 @@ def facts():
     visited_cities = len(city_repository.total_visited_cities())
     visited_sights = len(sight_repository.total_visited_sights())
 
-    return render_template("facts/index.html", total_countries=total_countries, total_cities=total_cities, total_sights=total_sights, visited_countries=visited_countries, visited_cities=visited_cities,
+    return render_template("summary/index.html", total_countries=total_countries, total_cities=total_cities, total_sights=total_sights, visited_countries=visited_countries, visited_cities=visited_cities,
     visited_sights=visited_sights)
